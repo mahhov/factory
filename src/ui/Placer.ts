dd388595f9a1428559bb6528b9072e1fe81889e1
mahhov1 <mahhov1@gmail.com>
2025-08-15 12:28:25 -0700

    bridge sprite

--- src/ui/Placer.ts
+++ src/ui/Placer.ts
@@ -3,7 +3,7 @@ import Camera from '../Camera.js';
 import Color from '../graphics/Color.js';
 import Painter from '../graphics/Painter.js';
 import Vector from '../util/Vector.js';
-import {Conveyor, Distributor, Empty, Entity, Extractor, GlassFactory, Junction, MegaFactory, Source, Void, Wall} from '../world/Entity.js';
+import {Bridge, Conveyor, Distributor, Empty, Entity, Extractor, GlassFactory, Junction, MegaFactory, Source, Void, Wall} from '../world/Entity.js';
 import {Rotation, RotationUtils} from '../world/Rotation.js';
 import {World, WorldLayer} from '../world/World.js';
 import {Input} from './Input.js';
@@ -14,7 +14,7 @@ enum State {
 
 export default class Placer {
 	static readonly State = State;
-	static readonly entityClasses: typeof Entity[] = [Wall, Conveyor, Distributor, Junction, Extractor, Source, Void, GlassFactory, MegaFactory];
+	static readonly entityClasses: typeof Entity[] = [Wall, Conveyor, Distributor, Junction, Bridge, Extractor, Source, Void, GlassFactory, MegaFactory];
 
 	private readonly painter: Painter;
 	private readonly camera: Camera;
