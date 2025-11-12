d2103e271cd43cad13d7994c9693e9cc966a80e2
mahhov1 <mahhov1@gmail.com>
2025-11-12 09:14:14 -0800

    tune terrain generation

--- src/util/Noise.ts
+++ src/util/Noise.ts
@@ -7,15 +7,24 @@ import Vector from './Vector.js';
 let noise = createNoise2D();
 
 // todo use metadata.md
-let resourceSettings = [
-	{resource: Material.IRON, scale: 0.05, threshold: 0.70, offset: 0},
-	{resource: Material.FLUX_SAND, scale: 0.02, threshold: 0.85, offset: 100},
-	{resource: Material.SULPHUR, scale: 0.05, threshold: 0.88, offset: 200},
-	{resource: Material.TITANIUM, scale: 0.08, threshold: 0.75, offset: 300},
-	{resource: Material.GRAPHITE, scale: 0.08, threshold: 0.90, offset: 400},
+enum Scale {
+	SMALL = .08,
+	MEDIUM = .05,
+	LARGE = .02,
+}
 
-	{resource: Liquid.WATER, scale: 0.05, threshold: 0.85, offset: 500, isLiquid: true},
-	{resource: Liquid.METHANE, scale: 0.08, threshold: 0.90, offset: 600, isLiquid: true},
+enum Frequency {
+}
+
+let resourceSettings = [
+	{resource: Material.IRON, scale: Scale.MEDIUM, threshold: 0.80, offset: 0},
+	{resource: Material.FLUX_SAND, scale: Scale.LARGE, threshold: 0.85, offset: 100},
+	{resource: Material.SULPHUR, scale: Scale.MEDIUM, threshold: 0.88, offset: 200},
+	{resource: Material.TITANIUM, scale: Scale.SMALL, threshold: 0.8, offset: 300},
+	{resource: Material.GRAPHITE, scale: Scale.SMALL, threshold: 0.90, offset: 400},
+
+	{resource: Liquid.WATER, scale: Scale.MEDIUM, threshold: 0.85, offset: 500, isLiquid: true},
+	{resource: Liquid.METHANE, scale: Scale.SMALL, threshold: 0.90, offset: 600, isLiquid: true},
 ];
 
 export let generateTerrain = (terrainLayer: GridWorldLayer<Entity>) => {
