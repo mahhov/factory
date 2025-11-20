9b02f5b1d55bb074253890483fb8d5fdc728df3a
mahhov1 <mahhov1@gmail.com>
2025-11-19 21:00:35 -0800

    stop entity tick if entity has been removed

--- src/world/Entity.ts
+++ src/world/Entity.ts
@@ -136,12 +136,13 @@ export class Entity implements Tileable {
 	}
 
 	tick(world: World, tile: Tile<Entity>) {
-		this.attributes.forEach(attributeChain => {
+		for (let attributeChain of this.attributes) {
+			if (tile.tileable !== this) return;
 			if (attributeChain.length === 1)
 				attributeChain[0].tickHelper(world, tile);
 			else if (attributeChain.every(attribute => attribute.tick(world, tile)))
 				attributeChain.forEach(attribute => attribute.reset());
-		});
+		}
 	}
 }
 
