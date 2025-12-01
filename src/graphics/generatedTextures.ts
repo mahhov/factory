a7858edf26240db13aecd7d9203cec183c3b2f16
mahhov1 <mahhov1@gmail.com>
2025-12-01 09:53:41 -0800

    pump textures

--- src/graphics/generatedTextures.ts
+++ src/graphics/generatedTextures.ts
@@ -1231,11 +1231,71 @@ let wallTextures = {
 		[4, 4, 8, 8, colors.backgroundGrey],
 	]]),
 };
+let liquidTextures = {
+	pump: new AnimatedGeneratedTextures(16, [[
+		[1, 0, 14, 1, colors.wallGrey],
+		[1, 15, 14, 1, colors.wallGrey],
+		[0, 1, 1, 14, colors.wallGrey],
+		[15, 1, 1, 14, colors.wallGrey],
+		[0, 0, 1, 1, colors.cornerGrey],
+		[15, 0, 1, 1, colors.cornerGrey],
+		[0, 15, 1, 1, colors.cornerGrey],
+		[15, 15, 1, 1, colors.cornerGrey],
+		[5, 6, 6, 4, colors.tier2],
+		[6, 5, 4, 6, colors.tier2],
+		[6, 6, 4, 4, colors.black],
+	]]),
+	poweredPump: new AnimatedGeneratedTextures(16, [[
+		[1, 0, 14, 1, colors.wallGrey],
+		[1, 15, 14, 1, colors.wallGrey],
+		[0, 1, 1, 14, colors.wallGrey],
+		[15, 1, 1, 14, colors.wallGrey],
+		[0, 0, 1, 1, colors.cornerGrey],
+		[15, 0, 1, 1, colors.cornerGrey],
+		[0, 15, 1, 1, colors.cornerGrey],
+		[15, 15, 1, 1, colors.cornerGrey],
+		[4, 5, 8, 6, colors.tier3],
+		[5, 4, 6, 8, colors.tier3],
+		[5, 5, 6, 6, colors.black],
+	]]),
+
+	// --- T3/T4: well (24x24) ---
+	well: new AnimatedGeneratedTextures(24, [[
+		// 1. Solid Base
+		[0, 0, 24, 24, colors.backgroundGrey],
+
+		// 2. Corner Supports (1x1 at the edge)
+		[0, 0, 1, 1, colors.cornerGrey],
+		[23, 0, 1, 1, colors.cornerGrey],
+		[0, 23, 1, 1, colors.cornerGrey],
+		[23, 23, 1, 1, colors.cornerGrey],
+
+		// 3. Walls (1px thick)
+		[1, 0, 22, 1, colors.wallGrey],
+		[1, 23, 22, 1, colors.wallGrey],
+		[0, 1, 1, 22, colors.wallGrey],
+		[23, 1, 1, 22, colors.wallGrey],
+
+		// 4. Well Casing (Stone/Earth color)
+		[2, 2, 20, 20, '#664422'], // Earth/stone color
+
+		// 5. Liquid Source (Deep central water pool)
+		[4, 4, 16, 16, colors.water],
+
+		// 6. Extraction/Lifting Mechanism (Heavy crossing bars)
+		[11, 2, 2, 20, colors.axilGrey], // Vertical lift
+		[2, 11, 20, 2, colors.axilGrey], // Horizontal support
+
+		// 7. Well Opening (Small hole in the center of the mechanism)
+		[10, 10, 4, 4, colors.wallGrey],
+	]]),
+};
 Object.entries(extractorTextures).forEach(([x, y]) => animatedGeneratedTextures[x] = y);
 Object.entries(factoryTextures).forEach(([x, y]) => animatedGeneratedTextures[x] = y);
 Object.entries(powerGeneratorTextures).forEach(([x, y]) => animatedGeneratedTextures[x] = y);
 Object.entries(ventTextures).forEach(([x, y]) => animatedGeneratedTextures[x] = y);
 Object.entries(wallTextures).forEach(([x, y]) => animatedGeneratedTextures[x] = y);
+Object.entries(liquidTextures).forEach(([x, y]) => animatedGeneratedTextures[x] = y);
 
 // todo material infra
 // todo power infra
